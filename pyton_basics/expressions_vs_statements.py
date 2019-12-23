2eec101afebe9210ff5f40dc4eddac1e3d925a8d
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 05:05:07 +0530

    Expressions vs Statements

---
+++ pyton_basics/expressions_vs_statements.py
@@ -0,0 +1,9 @@
+# Expression is a piece of code that produces a value
+# Statement is an entire line of code
+
+iq = 100
+user_age = iq / 5
+
+# Expression: iq / 5
+# Statement: user_age = iq / 5
+# Statement: iq = 100
