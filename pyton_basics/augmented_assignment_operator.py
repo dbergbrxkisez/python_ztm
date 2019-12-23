65385894affdac618f19096a16bdcfdbcdd08ec2
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 05:12:36 +0530

    Augmented Assignment Operator

---
+++ pyton_basics/augmented_assignment_operator.py
@@ -0,0 +1,14 @@
+# augmented assignment operator
+some_value = 5
+
+some_value = 5 + 2
+print(some_value)
+
+some_value = some_value + 3
+print(some_value)
+
+some_value += 5
+print(some_value)
+
+some_value -= 2
+print(some_value)
