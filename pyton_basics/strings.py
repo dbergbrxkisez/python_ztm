5bcb090f78810011933cc4d28b3bd6595a39e85e
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 05:18:53 +0530

    Strings

---
+++ pyton_basics/strings.py
@@ -0,0 +1,19 @@
+
+print(type("hi hello there 24!"))
+username = 'supercoder'
+password = 'supersecret'
+
+long_string = '''
+WOW
+O O
+---
+
+'''
+
+# print(long_string)
+
+first_name = 'first_name'
+last_name = 'last_name'
+full_name = first_name + ' ' + last_name
+
+print(full_name)
