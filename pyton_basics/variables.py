8466bda85c3f90256d1a4449e54f554628c4181d
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 05:01:35 +0530

    Variables

---
+++ pyton_basics/variables.py
@@ -0,0 +1,19 @@
+# constants - never change
+# capital means it should not be changed
+PI = 3.14
+
+# name code varibles to read like english
+iq = 190
+user_age = iq/4
+age = user_age
+
+
+print(age)
+
+
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+#  Good code = Readable + Understandable
