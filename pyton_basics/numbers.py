42dfab0e7e509663665ebeaf463f67f5dfcc83c7
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 04:39:17 +0530

    Numbers

---
+++ pyton_basics/numbers.py
@@ -0,0 +1,24 @@
+# Fundamental Data Types
+# int and float
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))  # 0.5
+
+print(type(0.00001))
+print(type(5.00001))
+print(type(10.56))
+
+print(type(0))
+
+print(type(20 + 1.1))
+
+# 2 * 2 * 2 = 8
+print(2 ** 3)
+
+# rounded down to integer
+print(2 // 4)
+print(5 // 4)
+
+# we get 2. 2 is the remainder
+print(6 % 4)
