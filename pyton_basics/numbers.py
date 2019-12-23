63a9492a6c009a2a8ed38d0fb5daa97b00e6752c
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 04:45:19 +0530

    Math Functions

--- pyton_basics/numbers.py
+++ pyton_basics/numbers.py
@@ -1,24 +1,31 @@
 # Fundamental Data Types
-# int and float
-print(type(2 + 4))
-print(type(2 - 4))
-print(type(2 * 4))
-print(type(2 / 4))  # 0.5
+####int and float
+# print(type(2 + 4))
+# print(type(2 - 4))
+# print(type(2 * 4))
+# print(type(2 / 4))  # 0.5
 
-print(type(0.00001))
-print(type(5.00001))
-print(type(10.56))
+# print(type(0.00001))
+# print(type(5.00001))
+# print(type(10.56))
 
-print(type(0))
+# print(type(0))
 
-print(type(20 + 1.1))
+# print(type(20 + 1.1))
 
 # 2 * 2 * 2 = 8
-print(2 ** 3)
+# print(2 ** 3)
 
 # rounded down to integer
-print(2 // 4)
-print(5 // 4)
+# print(2 // 4)
+# print(5 // 4)
 
 # we get 2. 2 is the remainder
-print(6 % 4)
+# print(6 % 4)
+
+# Math functions
+
+print(round(3.1))
+print(round(3.9))
+# abs is no -ve numbers
+print(abs(-20))
