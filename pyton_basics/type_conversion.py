22fe2b77c99fc96040f97126d167ed9e1419f7d5
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 05:26:25 +0530

    Type Conversion

---
+++ pyton_basics/type_conversion.py
@@ -0,0 +1,15 @@
+#  str = string
+# int = integer
+
+# converted 100 to str
+print(type(str(100)))
+
+# int to str to int and print
+print(type(int(str(100))))
+
+
+a = str(100)
+b = float(a)
+c = type(b)
+print(c)
+# Type Conversion
