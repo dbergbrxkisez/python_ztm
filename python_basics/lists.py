2cdac3004a40a0d08366515638ff322634f07e36
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 06:31:24 +0530

    Lists

---
+++ python_basics/lists.py
@@ -0,0 +1,19 @@
+# List is an orderd sequence of objects. That can be of any type.
+# inside of [] we can have different objects
+# In python lists are a form of array.(like arrays but not array's to be specific)
+# next to each other
+
+li = [1, 2, 3, 4, 5]
+li2 = ['a', 'b', 'c']
+li3 = [1, 2, 'a', True]
+
+# Data Structure
+# It's a way for us to organize information and data into let's say a folder, cupboard, box. etc.
+# A container around your data that different pros and cons of accessing, removing, updating data.
+
+
+amazon_cart = ['notebooks', 'sunglasses']
+print(amazon_cart[0])
+print(amazon_cart[1])
+# nothin exists... cannot be accessed.
+# print(amazon_cart[2])
