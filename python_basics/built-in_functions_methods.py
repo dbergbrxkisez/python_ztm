b9dd8281b36994f180443b840eca4b127d5e5ae3
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 06:08:33 +0530

    Built-in Functions + Methods

---
+++ python_basics/built-in_functions_methods.py
@@ -0,0 +1,31 @@
+# Built-in functions
+str()
+int()
+float()
+# type()
+print()
+
+
+# prints the length of string
+print(len('hellllooooo'))
+
+greet = 'hellllooooo'
+print(greet[:])
+print(greet[0:len(greet)])
+
+# Methods
+# it usually has a . in front of it.
+# .format() is a method
+
+
+quote = 'to be or not to be'
+quote2 = quote.replace('be', 'me')
+# .upper() .capitalize() is  a method
+print(quote.upper())
+print(quote.capitalize())
+
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
+
+print(quote2)
+print(quote)
