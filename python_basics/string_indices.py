6a1d419c3384432de007dc9a503e1ab0a5cc692c
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 05:49:53 +0530

    String Indices

---
+++ python_basics/string_indices.py
@@ -0,0 +1,14 @@
+# string is stored in memory as ordered sequence of char.
+# piece of text
+
+# pylint-disable - This disables the auto text formatting
+selfish = '01234567'
+#          01234567
+
+#  [start:stop:stepover]
+print(selfish[1:])
+print(selfish[:5])
+print(selfish[::1])
+print(selfish[-3])
+print(selfish[::-1])
+print(selfish[::-2])
