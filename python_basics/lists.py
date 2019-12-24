763ead485d25d4fcb3ccda85b6646c33807af2f0
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 06:37:14 +0530

    List Slicing

--- python_basics/lists.py
+++ python_basics/lists.py
@@ -12,8 +12,18 @@ li3 = [1, 2, 'a', True]
 # A container around your data that different pros and cons of accessing, removing, updating data.
 
 
-amazon_cart = ['notebooks', 'sunglasses']
+amazon_cart = ['notebooks', 'sunglasses', 'toys', 'grapes']
 print(amazon_cart[0])
 print(amazon_cart[1])
 # nothin exists... cannot be accessed.
 # print(amazon_cart[2])
+
+
+# List Slicing
+print(amazon_cart[0::2])
+
+amazon_cart[0] = 'laptop'
+new_cart = amazon_cart[:]      # : copies and == will point to the same
+new_cart[0] = 'gum'
+print(new_cart)
+print(amazon_cart)
