228f9ddbf356d7e1f1a9febe37f4fcc7c0561f77
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 06:58:30 +0530

    List Methods details updated

--- python_basics/list_methods.py
+++ python_basics/list_methods.py
@@ -2,20 +2,32 @@ basket = [1, 2, 3, 4, 5]
 
 # ADDING
 
+
 # just changing not updating
 # new_list = basket.append(100)
 
 # This updates the newly created list
 # After we have appended then we can assign basket to new_list so that it points to basket which points to newly modified list
+
 # basket.append(100)
+#basket.insert(4, 100)
+
 # new_list = basket
 # print(basket)
 
-# print(new_list)
-
 basket.extend([100])
 
+# print(new_list)
 
-# removing
+
+# REMOVING
+#
+
+# basket.pop()
+# basket.pop(0)
+# we give it the index
+# basket.remove(4)
+# we give it the value
 new_list = basket.clear()
+# clears th list completely
 print(basket)
