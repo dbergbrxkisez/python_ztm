1de7f8f3af726351174fe1f0bdec8910020ecf33
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 06:50:21 +0530

    Matrix

---
+++ python_basics/matrix.py
@@ -0,0 +1,13 @@
+# Matrix
+matrix = [
+    [1, 5, 1],
+    [0, 1, 0],
+    [1, 0, 1]
+]
+
+# access row 1 col 2 = 5
+print(matrix[0][1])
+
+
+# To be accessed as required
+# print(matrix[0][1][][][])
