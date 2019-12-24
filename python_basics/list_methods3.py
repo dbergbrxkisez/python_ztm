4e70da17a952c3105559d28dad22c9ad3e648ab2
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 07:15:35 +0530

    List Methods 3

---
+++ python_basics/list_methods3.py
@@ -0,0 +1,22 @@
+basket = ['a', 'x', 'b', 'c', 'd', 'e', 'd']
+# basket.sort()
+# print(basket)
+
+# print(sorted(basket))
+
+# copies and creates new sorted list
+# new_basket = basket[:]
+# new_basket.sort()
+# print(new_basket)
+# print(basket)
+
+# #copies and creates new sorted list
+# new_basket = basket.copy
+# new_basket.sort()
+# print(new_basket)
+# print(basket)
+
+# sorted reversed basket
+basket.sort()
+basket.reverse()
+print(basket)
