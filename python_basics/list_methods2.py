c8ad1809bc4550d199a3bc90c67731b04b37bb92
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 07:04:59 +0530

    List methods 2

---
+++ python_basics/list_methods2.py
@@ -0,0 +1,10 @@
+basket = ['a', 'b', 'c', 'd', 'e', 'd']
+
+print(basket.index('d', 0, 4))
+
+print('d' in basket)
+print('x' in basket)
+
+print('i' in 'hi my name is Ian')
+
+print(basket.count('d'))
