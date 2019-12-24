001c8eda0ce0fbc70f2b67dbc99d3bcb21ee9527
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 08:20:05 +0530

    Dictionary Methods 2

---
+++ python_basics/dictionary_methods.py
@@ -0,0 +1,33 @@
+# Dictionary
+user = {
+    'basket': [1, 2, 3],
+    'greet': 'hello',
+    'age': 20
+}
+
+# user2 = dict(name='JohnJohn')
+# print(user2)
+
+# print('basket' in user)
+# print('size' in user)
+
+# print('age' in user.keys())
+# print('hello' in user.values())
+
+# print(user.items())
+
+# user.clear()
+# print(user)
+
+
+# user2 = user.copy()
+# print(user.clear())
+# print(user2)
+
+# print(user.pop('age'))
+
+# print(user.popitem())
+# print(user)
+
+print(user.update({'age': 55}))
+print(user)
