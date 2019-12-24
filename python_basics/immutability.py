1cd4dab40be3d0f9d8c4b6dd32706b37248445e5
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 05:55:32 +0530

    Immutability

---
+++ python_basics/immutability.py
@@ -0,0 +1,15 @@
+# Strings are immutable
+selfish = '01234567'
+#          01234567
+
+selfish = 100
+
+# Not possible Strings are immutable
+# selfish[0] = '8'
+
+# completely new assignment is possible(WHOLE NEW STRING)
+# selfish = '8
+
+print(selfish)
+
+# Can't reassign part of a string once created. It exists like that.
