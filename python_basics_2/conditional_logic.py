e09c4fd166148564f80ea9caeeceb9c85b34e854
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-25 04:17:15 +0530

    Truthy vs Falsey

--- python_basics_2/conditional_logic.py
+++ python_basics_2/conditional_logic.py
@@ -1,12 +1,30 @@
-is_old = True
-is_licenced = True
+is_old = bool('Hello')
+is_licenced = bool(5)
 
-if is_old and is_licenced:
+
+# if is_old and is_licenced:
+#     print('you are old enough to drive, and you have a licence!')
+# # elif condition:
+# # elif condition:
+# else:
+#     print('you are not of age!')
+#     print('okoko')
+
+
+print(bool('hello'))
+print(bool(''))
+print(bool(5))
+print(bool(0))
+print(bool(None))
+# https://stackoverflow.com/questions/39983695/what-is-truthy-and-falsy-how-is-it-different-from-true-and-false
+
+# Truthy and Falsy
+
+password = '123'
+username = 'Johnny'
+
+if password and username:
     print('you are old enough to drive, and you have a licence!')
-
-# elif condition:
-# elif condition:
 else:
     print('you are not of age!')
-
-print('okoko')
+    print('okoko')
