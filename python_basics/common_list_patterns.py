93d57005bd1c55c64f10fbbaace33f75873632d7
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 07:25:25 +0530

    Common List Patterns

---
+++ python_basics/common_list_patterns.py
@@ -0,0 +1,25 @@
+basket = ['a', 'x', 'b', 'c', 'd', 'e', 'd']
+# basket.sort()
+# basket.reverse()
+# print(len(basket))
+
+# reverse
+# print(basket[::-1])
+
+# make a copy of the list
+# print(basket[:])
+
+# range from start to stop which is 1 to 99
+# print(list(range(1, 100)))
+
+# range starting from 0 to 99
+# print(list(range(100)))
+
+# range starting from 0 to 100
+# print(list(range(101)))
+
+# string method .join() joins iteratable items and joins to it
+sentence = '!'
+new_sentence = sentence.join(['hi', 'my', 'name', 'is', 'JOJ'])
+
+print(new_sentence)
