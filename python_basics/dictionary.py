8b58d96912635160196c6aaf4c0817a120f78e87
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-24 07:40:59 +0530

    Dictionary

---
+++ python_basics/dictionary.py
@@ -0,0 +1,24 @@
+# Dictionary
+# other languages: map, hash table...
+# in python it's Dict
+dictionary = {
+    'a': [1, 2, 3],
+    'b': 'hello',
+    'x': True
+}
+
+my_list = [
+    {
+        'a': [1, 2, 3],
+        'b': 'hello',
+        'x': True
+    },
+    {
+        'a': [4, 5, 6],
+        'b': 'hello',
+        'x': True
+    }
+]
+
+print(my_list[0]['a'][2])
+print(dictionary['a'][1])
