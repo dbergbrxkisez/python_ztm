25ae2f613ae8f1c5faa0dbd7abef003acbe14995
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-25 16:12:38 +0530

    Clean Code

---
+++ python_basics_2/clean_code.py
@@ -0,0 +1,24 @@
+# clean code
+# def is_even(num):
+#     if num % 2 == 0:
+#         return True
+#     elif num % 2 != 0:
+#         return False
+
+
+# def is_even(num):
+#     if num % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# def is_even(num):
+#     if num % 2 == 0:
+#         return True
+#     return False
+
+def is_even(num):
+    return num % 2 == 0
+
+
+print(is_even(50))
