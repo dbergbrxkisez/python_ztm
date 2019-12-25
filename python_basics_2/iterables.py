02a5b01407369dda1c022c2102f227de47bdcbaf
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-25 05:49:09 +0530

    Iterables

---
+++ python_basics_2/iterables.py
@@ -0,0 +1,17 @@
+user = {
+    'name': 'Golem',
+    'age': 5006,
+    'can_swim': False
+}
+
+for key, value in user.items():
+    print(key, value)
+
+for item in user.values():
+    print(item)
+
+for item in user.keys():
+    print(item)
+
+#iterable - list, dictionary, tuple, set, string
+# iterate -> one by one check each item in the collection.
