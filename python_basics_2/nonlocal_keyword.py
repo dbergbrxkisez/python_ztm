c76f8fcba24c30d52a00d817379a2b086e6bb85d
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-25 20:08:25 +0530

    nonlocal Keyword

---
+++ python_basics_2/nonlocal_keyword.py
@@ -0,0 +1,19 @@
+# Scope = What variables do I have access to?
+def outer():
+    x = "local"
+
+    def inner():
+        nonlocal x
+        x = "nonlocal"
+        print("inner:", x)
+
+    inner()
+
+    print("outer:", x)
+
+
+outer()
+# 1 - start with local
+# 2 - Parent local?
+# 3 - Global
+# 4 - built in python functions.
