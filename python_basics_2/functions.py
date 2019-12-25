5658c55b54c117771210bcee69a513fb3519ce74
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-25 15:27:07 +0530

    Default Parameters and Default Arguments

--- python_basics_2/functions.py
+++ python_basics_2/functions.py
@@ -1,9 +1,18 @@
 # parameters | used on defining the function
-def say_hello(name, emoji):
+# Default Parameters | assigns a default value if not defined
+
+
+def say_hello(name='Darth Vader', emoji='😈'):
     print(f'helllloooooooo {name}{emoji}')
 
 
 # arguments | used when we call/invoke the function
+# positional arguments | are arguments that require to be in proper position
 say_hello('D', '😋')
 say_hello('Dan', '😋')
 say_hello('Emily', '😋')
+
+# keyword arguments | this is bad practice
+say_hello(emoji='😋', name='Bibi')
+say_hello()
+say_hello('Timmy')
