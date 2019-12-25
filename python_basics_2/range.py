18133e0b50ac7c511abdfe490b1dfe50c4ad3991
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-25 06:15:49 +0530

    Range()

---
+++ python_basics_2/range.py
@@ -0,0 +1,20 @@
+# range() function is an object that return a object
+
+print(range(100))
+
+# for _ in range(0, 10):
+#     print(_)
+
+# for _ in range(0, 10, 2):
+#     print(_)
+
+# for _ in range(10, 0, -1):
+#     print(_)
+
+# for _ in range(10, 0, -2):
+#     print(_)
+
+
+# create a list that has integers
+for _ in range(2):
+    print(list(range(10)))
