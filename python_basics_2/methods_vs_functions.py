b189f784d09136273d8613c03a06aa4d5b13c06a
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-25 15:58:03 +0530

    Methods vs Functions

---
+++ python_basics_2/methods_vs_functions.py
@@ -0,0 +1,18 @@
+# Methods vs Functions
+
+# Built-in Functions
+# list()
+# print()
+# max()
+# min()
+# input()
+
+# Custom Functions
+# def some_random_stuff():
+#     pass
+# some_random_stuff()
+
+# Methods | has to be owned by something(object or data type) before dot(.) | They are built in objects
+# .something()
+# print('Hellooo')
+# capitalize() | GIVES ERROR/Not function,
