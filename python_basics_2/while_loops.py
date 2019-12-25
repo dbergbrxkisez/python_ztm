5736940aa274260915b0f01fa2c55b8034894a48
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-25 06:44:59 +0530

    While Loops 2

--- python_basics_2/while_loops.py
+++ python_basics_2/while_loops.py
@@ -5,3 +5,16 @@ while i < 50:
     break
 else:
     print('done with all the work')
+
+my_list = [1, 2, 3]
+for item in my_list:
+    print(item)
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+
+while True:
+    response = input('say something: ')
+    if(response == 'bye'):
+        break
