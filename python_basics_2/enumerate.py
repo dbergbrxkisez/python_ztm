e0327660a0fcdd1d8f02ba23ac3f509d433ebd0e
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-25 06:24:38 +0530

    Enumerate

---
+++ python_basics_2/enumerate.py
@@ -0,0 +1,16 @@
+# for char in enumerate('Hellllooo'):
+#     print(char)
+
+
+# for i, char in enumerate([1, 2, 3]):
+#     print(i, char)
+
+# for i, char in enumerate([1, 2, 3]):
+#     print(i, char)
+
+
+for i, char in enumerate(list(range(100))):
+    print(i, char)
+
+    if char == 50:
+        print(f'index of 50 is: {i}')
