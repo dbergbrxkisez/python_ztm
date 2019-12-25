740cfc14a6aa3293a31c39574ddc9ff4b59f1f5c
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-25 14:12:21 +0530

    Developer Fundamentals IV

--- python_basics_2/our_first_GUI.py
+++ python_basics_2/our_first_GUI.py
@@ -1,3 +1,8 @@
+# clean
+# Readability
+# predictability
+# DRY
+
 # Exercise!
 picture = [
     [0, 0, 0, 1, 0, 0, 0],
@@ -12,11 +17,18 @@ picture = [
 # if 0 -> print ''
 # if 1 -> print *
 
+
+fill = '*'
+empty = ''
 for row in picture:
     for pixel in row:
-        if (pixel == 1):
-            print('*', end=" ")
+        if (pixel):               # Truthy value either 0 or 1
+            print(fill, end=" ")
+            print(fill, end=" ")
+            print(fill, end=" ")
         else:
-            print(' ', end=" ")
+            print(empty, end=" ")
+            print(empty, end=" ")
+            print(empty, end=" ")
 
     print('')  # need a new line after every row
