9a2cc9554b36495ccf787fa863d100b83200d722
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-26 13:45:32 +0530

    What is OOP Part 2

--- advance_python_oop/what_is_oop.py
+++ advance_python_oop/what_is_oop.py
@@ -1,4 +1,15 @@
 # OOP
+
+
+class BigObject:  # Class
+    # code
+    pass
+
+
+obj1 = BigObject()  # instanciate
+obj2 = BigObject()  # instanciate
+obj3 = BigObject()  # instanciate
+
 print(type(None))
 print(type(True))
 print(type(5))
@@ -7,3 +18,4 @@ print(type('hi'))
 print(type([]))
 print(type(()))
 print(type({}))
+print(type(obj1))
