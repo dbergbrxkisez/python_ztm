a6b732a652b0e6744142239ebcfb9ef809c987ef
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-26 20:04:18 +0530

    __init__

--- advance_python_oop/creating_our_own_objects.py
+++ advance_python_oop/creating_our_own_objects.py
@@ -3,27 +3,24 @@ class PlayerCharacter:
     # Class Object Attribute
     membership = True
 
-    def __init__(self, name, age):
-        if (PlayerCharacter.membership):
+    def __init__(self, name='anonymous', age=0):
+        if (age > 18):
             self.name = name  # attributes | Pieces of data that are dynamic
             self.age = age
 
     def shout(self):
         print(f'my name is {self.name}')
 
-    def run(self, hello):
-        print(f'my name is {self.name}')
 
+player1 = PlayerCharacter('Tom', 10)
+# player2 = PlayerCharacter()
+# player2.attack = 50
 
-player1 = PlayerCharacter('Cindy', 44)
-player2 = PlayerCharacter('Tom', 21)
-player2.attack = 50
-
-# print(player1.name)
+# print(player1.na`me)
 # print(player1.run())
 # print(player2.membership)
 # print(player2.age)
 # print(player2.attack)
-# print(player1.shout())
+print(player1.shout())
 # print(player2.shout())
-print(player1.run('hello'))
+# print(player1.run('hello'))
