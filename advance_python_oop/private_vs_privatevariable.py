82b2278a1f2dcd71ddc26815de361c77257ae51c
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-27 00:55:04 +0530

    Private vs Public Variables

---
+++ advance_python_oop/private_vs_privatevariable.py
@@ -0,0 +1,18 @@
+# private ?
+class PlayerCharacter:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    def run(self):
+        print('run')
+
+    def speak(self):
+        print(f'my name is {self._name}, and I am {self._age} years old')
+
+
+player1 = PlayerCharacter('andrei', 100)
+# player1.name = '!!!'
+# player1.speak = 'BOOOOO'
+
+print(player1.speak)
