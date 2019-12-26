875c73538d3a1b9fc719a891791cea97e04b925d
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-26 19:42:07 +0530

    Attributes and Methods

--- advance_python_oop/creating_our_own_objects.py
+++ advance_python_oop/creating_our_own_objects.py
@@ -1,20 +1,29 @@
 # OOP
 class PlayerCharacter:
-    def __init__(self, name, age):
-        self.name = name  # attributes
-        self.age = age
+    # Class Object Attribute
+    membership = True
 
-    def run(self):
-        print('run')
-        return 'done'
+    def __init__(self, name, age):
+        if (PlayerCharacter.membership):
+            self.name = name  # attributes | Pieces of data that are dynamic
+            self.age = age
+
+    def shout(self):
+        print(f'my name is {self.name}')
+
+    def run(self, hello):
+        print(f'my name is {self.name}')
 
 
 player1 = PlayerCharacter('Cindy', 44)
 player2 = PlayerCharacter('Tom', 21)
 player2.attack = 50
 
-print(player1)
-print(player1.run())
-print(player2)
-print(player2.age)
-print(player2.attack)
+# print(player1.name)
+# print(player1.run())
+# print(player2.membership)
+# print(player2.age)
+# print(player2.attack)
+# print(player1.shout())
+# print(player2.shout())
+print(player1.run('hello'))
