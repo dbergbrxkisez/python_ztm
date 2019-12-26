5893b4b9086bb5566107a7f3c77dd522a4a1cbb2
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-26 20:13:54 +0530

    @classmethod and @staticmethod

--- advance_python_oop/creating_our_own_objects.py
+++ advance_python_oop/creating_our_own_objects.py
@@ -3,16 +3,22 @@ class PlayerCharacter:
     # Class Object Attribute
     membership = True
 
-    def __init__(self, name='anonymous', age=0):
-        if (age > 18):
-            self.name = name  # attributes | Pieces of data that are dynamic
-            self.age = age
+    def __init__(self, name, age):
+        self.name = name  # attributes | Pieces of data that are dynamic
+        self.age = age
 
     def shout(self):
         print(f'my name is {self.name}')
 
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return cls('Teddy', num1+num2)
 
-player1 = PlayerCharacter('Tom', 10)
+    @staticmethod
+    def adding_thing(num1, num2):
+        return num1 + num2
+
+# player1 = PlayerCharacter('Tom', 10)
 # player2 = PlayerCharacter()
 # player2.attack = 50
 
@@ -21,6 +27,10 @@ player1 = PlayerCharacter('Tom', 10)
 # print(player2.membership)
 # print(player2.age)
 # print(player2.attack)
-print(player1.shout())
+# print(player1.shout())
 # print(player2.shout())
 # print(player1.run('hello'))
+
+
+player3 = PlayerCharacter.adding_things(2, 3)
+print(player3.age)
