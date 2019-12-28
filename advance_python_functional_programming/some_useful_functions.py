4ef6e6423ef80c125ee3cb01bb22f2e344289631
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-28 14:46:10 +0530

    filter()

--- advance_python_functional_programming/some_useful_functions.py
+++ advance_python_functional_programming/some_useful_functions.py
@@ -8,5 +8,10 @@ def multiply_by2(item):
     return item*2
 
 
-print(list(map(multiply_by2, my_list)))
+def only_odd(item):
+    return item % 2 != 0
+
+
+# print(list(map(multiply_by2, my_list)))
+print(list(filter(only_odd, my_list)))
 print(my_list)
