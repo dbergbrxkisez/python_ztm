adef0c55597289f6cacf18fa8a22cfde30ec2828
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-28 15:05:56 +0530

    reduce()

--- advance_python_functional_programming/some_useful_functions.py
+++ advance_python_functional_programming/some_useful_functions.py
@@ -1,9 +1,10 @@
 # map, filter, zip, and reduce
 
 #map(action, dataThatWeWannaTakeActionUpon)
+from functools import reduce
 my_list = [1, 2, 3]
-your_list = (10, 20, 30)
-their_list = (5, 4, 3)
+# your_list = (10, 20, 30)
+# their_list = (5, 4, 3)
 
 
 def multiply_by2(item):
@@ -14,8 +15,14 @@ def only_odd(item):
     return item % 2 != 0
 
 
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+
 # print(list(map(multiply_by2, my_list)))
 # print(list(filter(only_odd, my_list)))
-print(list(zip(my_list, your_list)))
+# print(list(zip(my_list, your_list)))
+print(reduce(accumulator, my_list, 0))
 
 print(my_list)
