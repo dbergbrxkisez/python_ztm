bea3cc9b1c05b0d86270836085f72f6cad325f86
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2019-12-28 14:53:59 +0530

    zip()

--- advance_python_functional_programming/some_useful_functions.py
+++ advance_python_functional_programming/some_useful_functions.py
@@ -2,6 +2,8 @@
 
 #map(action, dataThatWeWannaTakeActionUpon)
 my_list = [1, 2, 3]
+your_list = (10, 20, 30)
+their_list = (5, 4, 3)
 
 
 def multiply_by2(item):
@@ -13,5 +15,7 @@ def only_odd(item):
 
 
 # print(list(map(multiply_by2, my_list)))
-print(list(filter(only_odd, my_list)))
+# print(list(filter(only_odd, my_list)))
+print(list(zip(my_list, your_list)))
+
 print(my_list)
