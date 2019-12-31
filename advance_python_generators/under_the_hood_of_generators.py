5406327913b4d5ad25af3e57b81da0ca0ea9aae4
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2020-01-01 04:31:19 +0530

    Under the Hood Of Generators

---
+++ advance_python_generators/under_the_hood_of_generators.py
@@ -0,0 +1,36 @@
+# our own for loop
+def special_for(iterable):
+    iterator = iter(iterable)
+    while True:
+        try:
+            print(iterator)
+            print(next(iterator) * 2)
+        except StopIteration:
+            break
+
+
+special_for([1, 2, 3])
+
+
+# Our Own Range
+class MyGen():
+    current = 0
+
+    def __init__(self, first, last):
+        self.fitst = first
+        self.last = last
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if MyGen.current < self.last:
+            num = MyGen.current
+            MyGen.current += 1
+            return num
+        raise StopIteration
+
+
+gen = MyGen(0, 100)
+for i in gen:
+    print(i)
