652bc8311c1a0e523ee23513c4bc8564bcd57084
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2020-01-01 02:33:37 +0530

    Errors in Python

---
+++ advance_python_error_handling/error_in_python.py
@@ -0,0 +1,34 @@
+# Error Handling
+# It allows us to let the python script continue running even if there are errors
+# A user might give something bad that we wouldn't want so we need to handle errors
+# Built in Exceptions: https://docs.python.org/3/library/exceptions.html
+
+
+# TypeError: Adding Int and Str
+# print(1+'hello')
+
+# SyntaxError: Missing colon
+# def hoooohooo()
+#    pass
+
+# NameError: 'name' is not defined
+# def hooohooo():
+#     1 + name
+# hooohooo()
+
+# IndexError: list index out of range
+# def hooohooo():
+#    li = [1,2,3]
+#    li[5]
+# hooohooo()
+
+# KeyError: 'b'
+# def hooohooo():
+#    di = {'a':1}
+#    di['b']
+# hooohooo()
+
+# ZeroDivisionError: division by zero
+# def hooohooo():
+#    5/0
+# hooohooo()
