7ea8fd840322d3929f1ed307d30deb8ce69f4380
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2020-01-01 01:26:08 +0530

    Higher Order Function

--- advance_python_decorators/decorators.py
+++ advance_python_decorators/decorators.py
@@ -13,3 +13,16 @@ def greet():
 a = hello(greet)
 
 print(a)
+
+
+# Higher Order Function HOC
+# map() reduce() filter() are all HOC
+# It could either be a function that accepts another function inside of it's param or a function that returns another function.
+def greet2(func):
+    func()
+
+
+def greet3():
+    def func():
+        return 5
+    return func
