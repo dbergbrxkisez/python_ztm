2e36089f306ff319f1483de50e66499997f65751
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2020-01-01 23:49:05 +0530

    Different Ways to Import

--- modules/main.py
+++ modules/main.py
@@ -1,4 +1,7 @@
-import utility
-import shopping.shopping_cart
+from modules.utility import multiply, divide
+from modules.shopping.more_shopping import shopping_cart
 
-print(shopping.shopping_cart.buy('apple'))
+print(shopping_cart.buy('apple'))
+print(divide(5, 2))
+print(multiply(5, 2))
+print(max([1,2,3]))
