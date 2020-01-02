a49bfc12df2774a094ef38b1c3cb906766950d74
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2020-01-03 02:01:18 +0530

    File Paths

--- file_io/script.py
+++ file_io/script.py
@@ -1,3 +1,4 @@
+# https://docs.python.org/3/library/pathlib.html
 # print(my_file.read())
 # my_file.seek(0)
 # print(my_file.read())
@@ -13,16 +14,16 @@
 #
 #
 # read | This is a standard way to read the file.
-# with open('test.txt', mode='r') as my_file:
-#     print(my_file.readlines())
+with open('app/sad.txt', mode='r') as my_file:
+    print(my_file.read())
 
 # write | overrides the existing or creates new file.
 # with open('test.txt', mode='w') as my_file:
 #     text = my_file.write(':)')
 #     print(my_file.readlines())
-with open('sad.txt', mode='w') as my_file:
-    text = my_file.write(':(')
-    print(my_file.readlines())
+# with open('app/sad.txt', mode='w') as my_file:
+#     text = my_file.write(':(')
+#     print(my_file.readlines())
 
 # read and write
 # with open('test.txt', mode='r+') as my_file:
