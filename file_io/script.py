1754718c198a19c162cce82b2b35c7c4cfcacc8e
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2020-01-03 01:51:45 +0530

    Read, Write, Append

--- file_io/script.py
+++ file_io/script.py
@@ -1,5 +1,3 @@
-my_file = open('test.txt')
-
 # print(my_file.read())
 # my_file.seek(0)
 # print(my_file.read())
@@ -8,8 +6,30 @@ my_file = open('test.txt')
 # print(my_file.readline())
 # print(my_file.readline())
 # print(my_file.readline())
-
+#
 # a list that contains entire file and reads all the lines.
-print(my_file.readlines())
+# print(my_file.readlines())
+# my_file.close()
+#
+#
+# read | This is a standard way to read the file.
+# with open('test.txt', mode='r') as my_file:
+#     print(my_file.readlines())
 
-my_file.close()
+# write | overrides the existing or creates new file.
+# with open('test.txt', mode='w') as my_file:
+#     text = my_file.write(':)')
+#     print(my_file.readlines())
+with open('sad.txt', mode='w') as my_file:
+    text = my_file.write(':(')
+    print(my_file.readlines())
+
+# read and write
+# with open('test.txt', mode='r+') as my_file:
+#     text = my_file.write(':)')
+#     print(text)
+
+# append | appends to the end of the file.
+# with open('test.txt', mode='a') as my_file:
+#     text = my_file.write(':)')
+#     print(text)
