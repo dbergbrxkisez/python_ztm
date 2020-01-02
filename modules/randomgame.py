a1aa1f091c72fd4e5c94959a10a12a12965d7154
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2020-01-02 14:35:49 +0530

    updated sys.argv in Guessing Game

--- modules/randomgame.py
+++ modules/randomgame.py
@@ -2,17 +2,15 @@ from random import randint
 import sys
 
 # generate a number 1-10
-arg1 = int(sys.argv[1])
-arg2 = int(sys.argv[2])
-answer = randint(arg1, arg2)
+answer = randint(int(sys.argv[1]), int(sys.argv[2]))
 
 # input from user?
 # check that input is a number 1~10
 while True:
     try:
         print(answer)  # check the answer first
-        guess = int(input(f'guess a number {arg1}~{arg2}:  '))
-        if 0 < guess < 11:
+        guess = int(input(f'guess a number {int(sys.argv[1])}~{int(sys.argv[2])}:  '))
+        if int(sys.argv[1]) < guess < int(sys.argv[2]):
             if guess == answer:
                 print('you are a genius!')
                 break
