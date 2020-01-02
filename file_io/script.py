06e3b4df1b4ec824a471a32f02575781e12ac1ea
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2020-01-03 00:42:15 +0530

    Working With Files In Python

---
+++ file_io/script.py
@@ -0,0 +1,15 @@
+my_file = open('test.txt')
+
+# print(my_file.read())
+# my_file.seek(0)
+# print(my_file.read())
+# my_file.seek(0)
+#
+# print(my_file.readline())
+# print(my_file.readline())
+# print(my_file.readline())
+
+# a list that contains entire file and reads all the lines.
+print(my_file.readlines())
+
+my_file.close()
