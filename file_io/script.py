ee2f52aff6e0eb731faab234454d6e64a9af56d3
CAPTAIN <49444980+dbergbrxkisez@users.noreply.github.com>
2020-01-03 02:34:24 +0530

    Exercise Translator

--- file_io/script.py
+++ file_io/script.py
@@ -35,12 +35,12 @@
 #     text = my_file.write(':)')
 #     print(text)
 
-try:
-    with open('sad.txt', mode='x') as my_file:
-        print(my_file.read())
-except FileNotFoundError as err:
-    print('file does not exist')
-    raise err
-except IOError as err:
-    print('IO error')
-    raise err
+# try:
+#     with open('sad.txt', mode='x') as my_file:
+#         print(my_file.read())
+# except FileNotFoundError as err:
+#     print('file does not exist')
+#     raise err
+# except IOError as err:
+#     print('IO error')
+#     raise err
