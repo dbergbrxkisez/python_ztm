498b8426e98b69655664249be21e43afd90a6b89
D <dbergbrxkisez@DMAC.local>
2020-01-10 13:06:24 +0530

    Images With Python 3

--- scraping_with_python/image-playground/images.py
+++ scraping_with_python/image-playground/images.py
@@ -1,7 +1,8 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')
-filtered_img = img.convert('L')  # greyscale image.
-box = (100,100,400,400)
-region = filtered_img.crop(box)
-region.save("grey.png", 'png')
+img = Image.open('./astro.jpg')
+img.thumbnail((400,400))
+img.save('thubnail.jpg')
+
+
+print(img.size)
