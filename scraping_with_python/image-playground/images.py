328a96e6ce01d04ebdc26f9dd6a4c752c93f8322
CAPTAIN <dbergbrxkisez@DMAC.local>
2020-01-10 12:41:45 +0530

    Images with Python 2

--- scraping_with_python/image-playground/images.py
+++ scraping_with_python/image-playground/images.py
@@ -1,13 +1,7 @@
 from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/pikachu.jpg')
-# print(img)
-# print(img.format)
-# print(img.size)
-# print(img.mode)
-
-# print(dir(img))
-
-# filtered_img = img.filter(ImageFilter.SHARPEN)
 filtered_img = img.convert('L')  # greyscale image.
-filtered_img.save("grey.png", 'png')
+box = (100,100,400,400)
+region = filtered_img.crop(box)
+region.save("grey.png", 'png')
