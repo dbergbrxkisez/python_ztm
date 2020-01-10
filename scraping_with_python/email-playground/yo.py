c169cbe1264bc1a29e00dfe12d91fc8d69825837
dbergbrxkisez <dbergbrxkisez@gmail.com>
2020-01-10 22:15:13 +0530

    Error sending emails with python

--- scraping_with_python/email-playground/yo.py
+++ scraping_with_python/email-playground/yo.py
@@ -1,9 +1,10 @@
 import smtplib
 from email.message import EmailMessage
 
+
 email = EmailMessage()
-email['from '] = 'D'
-email['to'] = 'dkp8487@gmail.com'
+email['from '] = 'first_name last_name'
+email['to'] = 'testbyrandom@gmail.com'
 email['subject'] = 'You won 1,000,000 dollars!'
 
 email.set_content('I am a Python Master')
@@ -11,6 +12,6 @@ email.set_content('I am a Python Master')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('testemail.@gmail.com', 'testpassword')
+    smtp.login('testemail@gmail.com', 'testpassword')
     smtp.send_message(email)
     print('all good boss!')
