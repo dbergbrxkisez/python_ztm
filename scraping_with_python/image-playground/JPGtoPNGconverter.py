0a1a006cde9b707213fc947a3fb2b2f839eb8f74
dbergbrxkisez <dbergbrxkisez@gmail.com>
2020-01-10 15:36:26 +0530

    Solution JPG to PNG Converter

--- scraping_with_python/image-playground/JPGtoPNGconverter.py
+++ scraping_with_python/image-playground/JPGtoPNGconverter.py
@@ -3,12 +3,18 @@ import os
 from PIL import Image
 
 # grab first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
 
+print(image_folder, output_folder)
 
 # check if new/ exists, if not create it.
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
-
-# loop through Pokedex,
-# convert images to png
-# save to the new folder.
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png', 'png')
+    print('all done!')
 
