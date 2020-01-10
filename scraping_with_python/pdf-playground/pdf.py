867d10679651a921f874cee2a0e10a4ef8406dfa
dbergbrxkisez <dbergbrxkisez@gmail.com>
2020-01-10 16:17:03 +0530

    Exercise PDF Merger

--- scraping_with_python/pdf-playground/pdf.py
+++ scraping_with_python/pdf-playground/pdf.py
@@ -1,14 +1,28 @@
 #
 import PyPDF2
+import sys
 
-with open('dummy.pdf', 'rb') as file:
-    reader = PyPDF2.PdfFileReader(file)
-    # print(reader.numPages)
-    # print(reader.getPage(0))
-    page = reader.getPage(0)
-    page.rotateCounterClockwise(90)
-    writer = PyPDF2.PdfFileWriter()
-    writer.addPage(page)
-    with open('tilt.pdf', 'wb') as new_file:
-        writer.write(new_file)
+inputs = sys.argv[1:]
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        print(pdf)
+        merger.append(pdf)
+    merger.write('super.pdf')
+
+pdf_combiner(inputs)
+# python3 pdf.py dummy.py twopage.py tilt.py
+# This will combile all the PDFs.
+
+# with open('dummy.pdf', 'rb') as file:
+#     reader = PyPDF2.PdfFileReader(file)
+#     # print(reader.numPages)
+#     # print(reader.getPage(0))
+#     page = reader.getPage(0)
+#     page.rotateCounterClockwise(90)
+#     writer = PyPDF2.PdfFileWriter()
+#     writer.addPage(page)
+#     with open('tilt.pdf', 'wb') as new_file:
+#         writer.write(new_file)
 
