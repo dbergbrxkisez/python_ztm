9c11ec976ac11dabc0beb341fa3175353c6e0dba
dbergbrxkisez <dbergbrxkisez@gmail.com>
2020-01-11 14:11:52 +0530

    Password Checker 3

--- scraping_with_python/passwordchecker/checkmypass.py
+++ scraping_with_python/passwordchecker/checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -11,12 +12,37 @@ def request_api_data(query_char):
     return res
 
 
+def get_pasword_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
     # check password if it exists in API response
     # print(hashlib.sha1(password.encode('utf-8')).hexdigest().upper())
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    # print(response)
+    return get_pasword_leaks_count(response, tail)
 
 
 # request_api_data('123')
-pwned_api_check('123')
+# pwned_api_check('123')
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found {count} times... you should probably change your password.')
+        else:
+            print(f'{password} was NOT found. Carry on!')
+    return 'done'
+
+
+main(sys.argv[1:])
